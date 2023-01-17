77db80ad1cbe5d3ca0c86443f4ad9d0705576664
esra tansuk <esra.balkis@outlook.com>
2023-01-17 18:54:46 +0300

    ES6 Destructuring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+import animals, {useAnimals} from "./data";
+
+
+const [cat, dog] = animals;
+
+// const {name: catName, sound:catSound} = cat;
+
+// const {name, sound, feedingRequirements: {food, water}} = cat;
+
+// console.log(feedingRequirements);
+
+const [animal,makeSound] = useAnimals(cat);
+console.log(animal);
+makeSound();
+
+
+
+
+
+
+
+
+
 // CHALLENGE: uncomment the code below and see the car stats rendered
 // import React from "react";
 // import ReactDOM from "react-dom";
